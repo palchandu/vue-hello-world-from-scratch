ccfffd754e783b300be9e95e805891154291c871
palchandu <chandu2013pal@gmail.com>
2022-09-30 23:28:35 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+      <input type="text" v-model="message" />
+      <h3 class="message">{{ message }}</h3>
+      <h3>Some Other Message</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          message: 'Hello Universe',
+        };
+      },
+    };
+</script>
+
+<style>
+    #app {
+      font-size: 18px;
+      font-family: 'Roboto', sans-serif;
+      color: blue;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import {createApp} from 'vue';
+import App from './App.vue';
+
+// new Vue({
+//  render: h => h(App)
+// }).mount('#app')
+createApp(App).mount('#app');
